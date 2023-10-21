921df126699c9ad691188ba60a6152f397b49ff9
usern4445346 <user36536@mail.com>
2023-10-21 11:42:18 -0400

    comit the persist of phones but with out the usuario_id

--- src/main/java/com/nisum/appsample/model/entities/Telefono.java
+++ src/main/java/com/nisum/appsample/model/entities/Telefono.java
@@ -2,12 +2,15 @@ package com.nisum.appsample.model.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 import java.io.Serializable;
 import lombok.Data;
 
@@ -22,17 +25,16 @@ class Telefono implements Serializable {
 
     @Id
     @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.UUID)
 
-    private Long id;
+    private String id;
 
     @Column(name = "number")
     private String number;
     @Column(name = "citycode")
-    private String cityCode;
-    @Column(name = "countryCode")
-    private String countryCode;
-
+    private String citycode;
+    @Column(name = "countrycode")
+    private String contrycode;
     @ManyToOne
     @JoinColumn(name = "usuario_id")
     private Usuario usuario;
--- src/main/java/com/nisum/appsample/model/entities/Usuario.java
+++ src/main/java/com/nisum/appsample/model/entities/Usuario.java
@@ -4,17 +4,22 @@
  */
 package com.nisum.appsample.model.entities;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
 import java.io.Serializable;
 import java.util.List;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
+import java.util.UUID;
 import lombok.Data;
 import org.springframework.validation.annotation.Validated;
 
@@ -30,16 +35,15 @@ public class Usuario implements Serializable {
 
     @Id
     @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private String id;
     @Column(name = "name")
     private String name;
     @Column(name = "email")
-    @Email(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$",message = "El correo electrónico no es válido")
+    @Email(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "El correo electrónico no es válido")
     private String email;
     @Column(name = "password", nullable = true)
     private String password;
-    @OneToMany(mappedBy = "usuario")
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
     private List<Telefono> phones;
 
 }
