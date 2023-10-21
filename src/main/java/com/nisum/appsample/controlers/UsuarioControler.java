c62902c4101371e90a18b0f4f1e9f9091848104e
usern4445346 <user36536@mail.com>
2023-10-21 19:48:27 -0400

    new validation

--- src/main/java/com/nisum/appsample/controlers/UsuarioControler.java
+++ src/main/java/com/nisum/appsample/controlers/UsuarioControler.java
@@ -9,13 +9,19 @@ import com.nisum.appsample.model.entities.Telefono;
 import com.nisum.appsample.model.repository.UsuarioRepository;
 import com.nisum.appsample.model.services.UsuarioService;
 import jakarta.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.h2.jdbc.JdbcSQLIntegrityConstraintViolationException;
 /**
  *
  * @author hered
@@ -31,4 +37,14 @@ public class UsuarioControler {
         Usuario user = userservice.save(usuario);
         return ResponseEntity.ok(user);
     }
+
+    @ExceptionHandler(JdbcSQLIntegrityConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public Map<String, String> handleJdbcSQLIntegrityConstraintViolationException(JdbcSQLIntegrityConstraintViolationException ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "El correo electrónico ya está en uso");
+        return response;
+    }
+
 }
