a8c05bf63a9341b6bd2b08633cca7b57ea5df161
usern4445346 <user36536@mail.com>
2023-11-13 15:30:31 -0400

    fix the problem

--- src/main/java/com/nisum/appsample/domain/usuario/CreateUserService.java
+++ src/main/java/com/nisum/appsample/domain/usuario/CreateUserService.java
@@ -1,41 +1,18 @@
 package com.nisum.appsample.domain.usuario;
 
+import org.springframework.beans.factory.annotation.Autowired;
 
-import com.nisum.appsample.common.AutoCall;
-import com.nisum.appsample.common.UserMapper;
-import com.nisum.appsample.common.jwt.JwtGenerator;
-import com.nisum.appsample.common.password.PasswordGenerator;
 import com.nisum.appsample.infraestructure.usuario.UpdateUserPort;
-import com.nisum.appsample.infraestructure.usuario.entities.UsuarioEntity;
 import com.nisum.appsample.presentation.usuario.request.UsuarioRequest;
-import java.util.Date;
-import org.springframework.core.env.Environment;
 
 public class CreateUserService implements CreateUserPort {
 
-    @AutoCall
+    @Autowired
     UpdateUserPort updateUserPort;
-
-    @AutoCall
-    private Environment env;
 
     @Override
     public UsuarioRequest save(UsuarioRequest usuario) {
-        Date fec = new Date();
-
-        if (usuario.getId() == null) {
-            usuario.setCreated(fec);
-            usuario.setModified(fec);
-            usuario.setLastLogin(fec);
-        } else {
-            usuario.setModified(fec);
-        }
-        usuario.setToken(JwtGenerator.generateJWT(usuario.getEmail(), env.getProperty("secret"), 9000));
-        usuario.setIsactive(true);
-        usuario.setPassword(PasswordGenerator.cryptPass(usuario.getPassword(), env.getProperty("salt")));
-        UsuarioEntity ue = UserMapper.domainToEntity(usuario);
-        return UserMapper.entityToDomain(updateUserPort.save(ue));
-
+        return updateUserPort.save(usuario);
     }
 
 }
--- src/main/java/com/nisum/appsample/infraestructure/usuario/UpdateUserPort.java
+++ src/main/java/com/nisum/appsample/infraestructure/usuario/UpdateUserPort.java
@@ -1,9 +1,8 @@
 package com.nisum.appsample.infraestructure.usuario;
 
-import com.nisum.appsample.infraestructure.usuario.entities.UsuarioEntity;
 import com.nisum.appsample.presentation.usuario.request.UsuarioRequest;
 
 public interface UpdateUserPort {
-    public UsuarioEntity save(UsuarioEntity usuario);
+    public UsuarioRequest save(UsuarioRequest usuario);
 
 }
